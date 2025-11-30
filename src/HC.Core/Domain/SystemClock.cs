8be3899e5a4f8f1ed83dc02234cb76c2784d0707
Sidney Novais <sidneyrnovaes@gmail.com>
2025-11-30 14:35:22 -0300

    fix: _now property becomes frozen

--- src/HC.Core/Domain/SystemClock.cs
+++ src/HC.Core/Domain/SystemClock.cs
@@ -4,8 +4,8 @@ namespace HC.Core.Domain;
 
 public static class SystemClock
 {
-    private static DateTime _now = DateTime.UtcNow;
-    public static DateTime Now => _now;
+    private static DateTime _now;
+    public static DateTime Now => _now == DateTime.MinValue ? DateTime.UtcNow : _now;
     public static void Set(DateTime custom) => _now = custom;
-    public static void Clear() => _now = DateTime.UtcNow;
+    public static void Clear() => _now = DateTime.MinValue;
 }
