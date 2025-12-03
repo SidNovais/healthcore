7d5217fded4aaf916e23ab0aa9560d79090a21c3
Sidney Novais <sidneyrnovaes@gmail.com>
2025-12-03 20:29:56 -0300

    feat: dispatch domain events before committing to the database

--- src/HC.Core/Infrastructure/UnitOfWork.cs
+++ src/HC.Core/Infrastructure/UnitOfWork.cs
@@ -2,19 +2,23 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using HC.Core.Infrastructure.DomainEventsDispatching;
 
 namespace HC.Core.Infrastructure;
 
 public class UnitOfWork(
-    DbContext context
+    DbContext context,
+    IDomainEventsDispatcher domainEventsDispatcher
 ) : IUnitOfWork
 {
     private readonly DbContext _dbContext = context;
+    private readonly IDomainEventsDispatcher _domainEventsDispatcher = domainEventsDispatcher;
     public async Task<int> CommitAsync(
         Guid? internalCommandId,
         CancellationToken cancellationToken = default
     )
     {
+        await _domainEventsDispatcher.DispatchEventsAsync().ConfigureAwait(false);
         Task<int> task = _dbContext.SaveChangesAsync(cancellationToken);
         return await task.ConfigureAwait(false);
     }
