5db587da1051d10d0e9128b0037a0d8ccd55e697
Sidney Novais <sidneyrnovaes@gmail.com>
2025-12-19 19:45:30 -0300

    feat: Add OrderItemStatus

---
+++ src/HC.LIS/HC.LIS.Modules/TestOrders/Domain/Orders/OrderItemStatus.cs
@@ -0,0 +1,29 @@
+using HC.Core.Domain;
+
+namespace HC.LIS.Modules.TestOrders.Domain.Orders;
+
+public class OrderItemStatus : ValueObject
+{
+    public string Value { get; }
+    public static OrderItemStatus Requested => new("Requested");
+    public static OrderItemStatus OnHold => new("OnHold");
+    public static OrderItemStatus Accepted => new("Accepted");
+    public static OrderItemStatus InProgress => new("InProgress");
+    public static OrderItemStatus PartiallyCompleted => new("PartiallyCompleted");
+    public static OrderItemStatus Completed => new("Completed");
+    public static OrderItemStatus Rejected => new("Rejected");
+    public static OrderItemStatus Cancelled => new("Cancelled");
+    private OrderItemStatus(string value)
+    {
+        Value = value;
+    }
+    public static OrderItemStatus Of(string value) => new(value);
+    internal bool IsRequested => Value == "Requested";
+    internal bool IsOnHold => Value == "OnHold";
+    internal bool IsAccepted => Value == "Accepted";
+    internal bool IsInProgress => Value == "InProgress";
+    internal bool IsPartiallyCompleted => Value == "PartiallyCompleted";
+    internal bool IsCompleted => Value == "Completed";
+    internal bool IsRejected => Value == "Rejected";
+    internal bool IsCancelled => Value == "Cancelled";
+}
