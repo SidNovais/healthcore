f46b817bd3799f28698ba516377f46680fcc9b43
Sidney Novais <sidneyrnovaes@gmail.com>
2025-12-28 15:53:40 -0300

    test: Add PatientId, RequestedBy and OrderPriority to OrderSampleData

--- src/HC.LIS/HC.LIS.Modules/TestOrders/Tests/UnitTests/Orders/OrderSampleData.cs
+++ src/HC.LIS/HC.LIS.Modules/TestOrders/Tests/UnitTests/Orders/OrderSampleData.cs
@@ -6,6 +6,9 @@ namespace HC.LIS.Modules.TestOrders.UnitTests.Orders;
 public readonly struct OrderSampleData
 {
     public static readonly Guid OrderId = Guid.Parse("019b6642-6c05-7678-919a-2bd510a95e40");
+    public static readonly Guid PatientId = Guid.Parse("019b664c-52a4-7f37-a794-6da2481550c0");
+    public static readonly Guid RequestedBy = Guid.Parse("019b664c-79f0-7f45-87f3-84664a00e635");
+    public static readonly string OrderPriority = "Routine";
     public static readonly string SpecimenMnemonic = "BLD";
     public static readonly string MaterialType = "Whole Blood";
     public static readonly string ContainerType = "Vacutainer Tube";
