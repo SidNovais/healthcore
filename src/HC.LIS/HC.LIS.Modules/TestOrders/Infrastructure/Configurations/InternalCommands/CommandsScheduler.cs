606030830b39e97e7543d1f56a52a6fe5c4ee0db
Sidney Novais <sidneyrnovaes@gmail.com>
2026-01-10 14:01:42 -0300

    feat: Add CommandsScheduler

---
+++ src/HC.LIS/HC.LIS.Modules/TestOrders/Infrastructure/Configurations/InternalCommands/CommandsScheduler.cs
@@ -0,0 +1,61 @@
+using Dapper;
+using System.Data;
+using Newtonsoft.Json;
+using HC.Core.Domain;
+using HC.Core.Infrastructure.Data;
+using HC.Core.Infrastructure.InternalCommands;
+using HC.Core.Infrastructure.Serialization;
+using HC.LIS.Modules.TestOrders.Application.Configuration.Commands;
+using HC.LIS.Modules.TestOrders.Application.Contracts;
+
+namespace HC.LIS.Modules.TestOrders.Infrastructure.Configurations.InternalCommands;
+
+public class CommandsScheduler(
+    ISqlConnectionFactory sqlConnectionFactory,
+    IInternalCommandsMapper internalCommandsMapper
+) : ICommandsScheduler
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory = sqlConnectionFactory;
+
+    private readonly IInternalCommandsMapper _internalCommandsMapper = internalCommandsMapper;
+
+    public async Task EnqueueAsync(ICommand command)
+    {
+        IDbConnection? connection = _sqlConnectionFactory.GetConnection()
+        ?? throw new InvalidOperationException("Must exist connection to insert internal commands");
+
+        const string sqlInsert = @$"INSERT INTO ""test_orders"".""InternalCommands"" (""Id"", ""EnqueueDate"" , ""Type"", ""Data"") VALUES " +
+                                 "(@Id, @EnqueueDate, @Type, @Data)";
+
+        await connection.ExecuteAsync(sqlInsert, new
+        {
+            command.Id,
+            EnqueueDate = SystemClock.Now,
+            Type = _internalCommandsMapper.GetNameByType(command.GetType()),
+            Data = JsonConvert.SerializeObject(command, new JsonSerializerSettings
+            {
+                ContractResolver = new AllPropertiesContractResolver()
+            })
+        }).ConfigureAwait(false);
+    }
+
+    public async Task EnqueueAsync<T>(ICommand<T> command)
+    {
+        IDbConnection? connection = _sqlConnectionFactory.GetConnection()
+        ?? throw new InvalidOperationException("Must exist connection to insert internal commands");
+
+        const string sqlInsert = @$"INSERT INTO ""test_orders"".""InternalCommands"" (""Id"", ""EnqueueDate"" , ""Type"", ""Data"") VALUES " +
+                                 "(@Id, @EnqueueDate, @Type, @Data)";
+
+        await connection.ExecuteAsync(sqlInsert, new
+        {
+            command.Id,
+            EnqueueDate = SystemClock.Now,
+            Type = _internalCommandsMapper.GetNameByType(command.GetType()),
+            Data = JsonConvert.SerializeObject(command, new JsonSerializerSettings
+            {
+                ContractResolver = new AllPropertiesContractResolver()
+            })
+        }).ConfigureAwait(false);
+    }
+}
