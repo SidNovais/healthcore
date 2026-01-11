b59a438a6c498482d2a390cd83303a91f456f1f1
Sidney Novais <sidneyrnovaes@gmail.com>
2026-01-11 15:15:55 -0300

    feat: Add ProcessInternalCommandsJob

---
+++ src/HC.LIS/HC.LIS.Modules/TestOrders/Infrastructure/Configurations/InternalCommands/ProcessInternalCommandsJob.cs
@@ -0,0 +1,13 @@
+using Quartz;
+using HC.LIS.Modules.TestOrders.Infrastructure.Configurations.Processing;
+
+namespace HC.LIS.Modules.TestOrders.Infrastructure.Configurations.InternalCommands;
+
+[DisallowConcurrentExecution]
+public class ProcessInternalCommandsJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        await CommandsExecutor.Execute(new ProcessInternalCommandsCommand()).ConfigureAwait(false);
+    }
+}
