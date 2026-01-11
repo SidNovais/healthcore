4477a291ba93846775ed50dcfe946da80eb72b1e
Sidney Novais <sidneyrnovaes@gmail.com>
2026-01-11 14:43:00 -0300

    feat: Add ProcessInternalCommandsCommand

---
+++ src/HC.LIS/HC.LIS.Modules/TestOrders/Infrastructure/Configurations/InternalCommands/ProcessInternalCommandsCommand.cs
@@ -0,0 +1,8 @@
+using HC.LIS.Modules.TestOrders.Application.Contracts;
+
+namespace HC.LIS.Modules.TestOrders.Infrastructure.Configurations.InternalCommands;
+
+internal class ProcessInternalCommandsCommand : CommandBase, IRecurringCommand
+{
+}
+
