2fa35e3fa64efcf05d960e4253c12edc092a3f00
Sidney Novais <sidneyrnovaes@gmail.com>
2026-01-14 18:36:17 -0300

    feat: Add AuthenticationModule

---
+++ src/HC.LIS/HC.LIS.Modules/TestOrders/Infrastructure/Configurations/Authentication/AuthenticationModule.cs
@@ -0,0 +1,9 @@
+using Autofac;
+
+namespace HC.LIS.Modules.TestOrders.Infrastructure.Configurations.Authentication;
+
+internal class AuthenticationModule : Autofac.Module
+{
+    protected override void Load(ContainerBuilder builder)
+    { }
+}
