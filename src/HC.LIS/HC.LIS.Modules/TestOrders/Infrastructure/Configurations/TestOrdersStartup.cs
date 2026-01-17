d283779a589be6116d89a5b5b8da78142ef0eb2b
Sidney Novais <sidneyrnovaes@gmail.com>
2026-01-16 21:07:30 -0300

    feat: Add TestOrdersStartup

---
+++ src/HC.LIS/HC.LIS.Modules/TestOrders/Infrastructure/Configurations/TestOrdersStartup.cs
@@ -0,0 +1,67 @@
+using Autofac;
+using HC.Core.Application;
+using HC.Core.Infrastructure;
+using HC.Core.Infrastructure.EventBus;
+using HC.LIS.Modules.TestOrders.Infrastructure.Configurations.Authentication;
+using HC.LIS.Modules.TestOrders.Infrastructure.Configurations.DataAccess;
+using HC.LIS.Modules.TestOrders.Infrastructure.Configurations.EventBus;
+using HC.LIS.Modules.TestOrders.Infrastructure.Configurations.Logging;
+using HC.LIS.Modules.TestOrders.Infrastructure.Configurations.Mediation;
+using HC.LIS.Modules.TestOrders.Infrastructure.Configurations.Processing;
+using HC.LIS.Modules.TestOrders.Infrastructure.Configurations.Processing.Outbox;
+using HC.LIS.Modules.TestOrders.Infrastructure.Configurations.Quartz;
+using Serilog;
+
+namespace HC.LIS.Modules.TestOrders.Infrastructure.Configurations;
+
+public class TestOrdersStartup
+{
+    private static IContainer _container;
+    public static void Initialize(
+      string databaseConnectionString,
+      IExecutionContextAccessor executionContextAccessor,
+      ILogger logger,
+      IEventsBus? eventBus,
+      long? internalProcessingPoolingInterval = null
+    )
+    {
+        ILogger moduleLogger = logger.ForContext("Module", "TestOrders");
+        ConfigureContainer(
+          databaseConnectionString,
+          executionContextAccessor,
+          moduleLogger,
+          eventBus
+        );
+        QuartzStartup.Initialize(moduleLogger, internalProcessingPoolingInterval);
+        EventsBusStartup.Initialize(moduleLogger);
+    }
+
+    private static void ConfigureContainer(
+      string databaseConnectionString,
+      IExecutionContextAccessor executionContextAccessor,
+      ILogger logger,
+      IEventsBus? eventsBus
+    )
+    {
+        var containerBuilder = new ContainerBuilder();
+        containerBuilder.RegisterModule(new LoggingModule(logger));
+        containerBuilder.RegisterModule(new DataAccessModule(databaseConnectionString));
+        containerBuilder.RegisterModule(new ApplicationModule());
+        containerBuilder.RegisterModule(new ProcessingModule());
+        containerBuilder.RegisterModule(new EventsBusModule(eventsBus));
+        containerBuilder.RegisterModule(new MediatorModule());
+        containerBuilder.RegisterModule(new AuthenticationModule());
+        var domainNotificationsMap = new BiMap();
+        containerBuilder.RegisterModule(new OutboxModule(domainNotificationsMap));
+        BiMap internalCommandsMap = new();
+        containerBuilder.RegisterModule(new InternalCommandsModule(internalCommandsMap));
+        containerBuilder.RegisterModule(new QuartzModule());
+        containerBuilder.RegisterInstance(executionContextAccessor);
+        _container = containerBuilder.Build();
+        TestOrdersCompositionRoot.SetContainer(_container);
+    }
+    public static void Stop()
+    {
+        QuartzStartup.StopQuartz();
+    }
+}
