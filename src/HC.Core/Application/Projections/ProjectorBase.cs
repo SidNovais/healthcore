33000db5046276d85355bcfe7e98878ceaea2110
Sidney Novais <sidneyrnovaes@gmail.com>
2026-02-12 20:45:00 -0300

    fix: Set ProjectorBase as public

--- src/HC.Core/Application/Projections/ProjectorBase.cs
+++ src/HC.Core/Application/Projections/ProjectorBase.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace HC.Core.Application.Projections;
 
-internal abstract class ProjectorBase
+public abstract class ProjectorBase
 {
     protected static Task When(IDomainEvent domainEvent)
     {
