011a248c08c708cad70e60eb50dcd46b0c0d0fba
Sidney Novais <sidneyrnovaes@gmail.com>
2026-04-30 07:55:03 -0300

    refactor(integration-events): GetCollectionRequestFromSampleCollectionProbe uses module facade

--- src/HC.LIS/HC.LIS.Tests/IntegrationEvents/Probes/GetCollectionRequestFromSampleCollectionProbe.cs
+++ src/HC.LIS/HC.LIS.Tests/IntegrationEvents/Probes/GetCollectionRequestFromSampleCollectionProbe.cs
@@ -1,30 +1,24 @@
 using System;
 using System.Threading.Tasks;
-using Dapper;
 using HC.Core.IntegrationTests.Probing;
+using HC.LIS.Modules.SampleCollection.Application.Collections.GetCollectionRequestByPatientId;
 using HC.LIS.Modules.SampleCollection.Application.Collections.GetCollectionRequestDetails;
-using Npgsql;
+using HC.LIS.Modules.SampleCollection.Application.Contracts;
 
 namespace HC.LIS.Tests.IntegrationEvents.Probes;
 
 public sealed class GetCollectionRequestFromSampleCollectionProbe(
     Guid patientId,
-    string connectionString
+    ISampleCollectionModule module
 ) : IProbe<CollectionRequestDetailsDto>
 {
     public string DescribeFailureTo() =>
         $"CollectionRequest for PatientId {patientId} not found in SampleCollection";
 
-    public async Task<CollectionRequestDetailsDto?> GetSampleAsync()
-    {
-        using var connection = new NpgsqlConnection(connectionString);
-        return await connection.QueryFirstOrDefaultAsync<CollectionRequestDetailsDto>(
-            @"SELECT ""Id"" AS ""CollectionRequestId"", ""PatientId"", ""Status"", ""ArrivedAt""
-              FROM sample_collection.""CollectionRequestDetails""
-              WHERE ""PatientId"" = @PatientId",
-            new { PatientId = patientId }
-        ).ConfigureAwait(false);
-    }
+    public async Task<CollectionRequestDetailsDto?> GetSampleAsync() =>
+        await module
+            .ExecuteQueryAsync(new GetCollectionRequestByPatientIdQuery(patientId))
+            .ConfigureAwait(false);
 
     public bool IsSatisfied(CollectionRequestDetailsDto? sample) => sample is not null;
 }
